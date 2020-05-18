94aff7bf736f08fee76a4f163903b4865c15e662
Alexandru Iacoboae <alexiacoboae@gmail.com>
2020-05-18 08:37:18 +0300

    Adding Code Base

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace Xand0ConsoloGameCSharp
     {
         static void Main(string[] args)
         {
+            Game game = new Game();
+            game.PlayGame();
+            Console.WriteLine("Game over");
         }
     }
 }
---
+++ Square.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xand0ConsoloGameCSharp
+{
+    public enum Player { Noone = 0, Noughts, Crosses}
+
+    public struct Square
+    {
+        public Player Owner { get; }
+
+        public Square(Player owner)
+        {
+            this.Owner = owner;
+        }
+
+        public override string ToString()
+        {
+            switch (Owner)
+            {
+                case Player.Noone:
+                    return ".";
+                    
+                case Player.Noughts:
+                    return "O";
+                case Player.Crosses:
+                    return "X";
+                default:
+                    throw new Exception("Invalid State");
+            }
+        }
+
+
+    }
+}
